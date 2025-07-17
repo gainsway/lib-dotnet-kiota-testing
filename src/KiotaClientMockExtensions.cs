cef4d7622c9866817d654fda6499318f0db17c91
Peter Potucek <peter.potucek@gainsway.com>
2025-07-17 18:41:34 +0100

    feat: mock failures

--- src/KiotaClientMockExtensions.cs
+++ src/KiotaClientMockExtensions.cs
@@ -210,4 +210,158 @@ public static class KiotaClientMockExtensions
             )
             .Returns(returnValue);
     }
+
+    /// <summary>
+    /// Mocks an exception for a Kiota client request that returns a single object.
+    /// </summary>
+    /// <typeparam name="T">The type of the Kiota client request builder.</typeparam>
+    /// <typeparam name="R">The type of the response object.</typeparam>
+    /// <param name="mockedClient">The mocked client request builder instance.</param>
+    /// <param name="urlTemplate">The URL template to match the request.</param>
+    /// <param name="exception">The exception to throw when the request is made.</param>
+    /// <param name="requestInfoPredicate">
+    /// An optional predicate to further filter the request information.
+    /// </param>
+    public static void MockClientResponseException<T, R>(
+        this T mockedClient,
+        string urlTemplate,
+        Exception exception,
+        Expression<Predicate<RequestInformation>>? requestInfoPredicate = null
+    )
+        where T : BaseRequestBuilder
+        where R : IParsable
+    {
+        var requestAdapter = GetRequestAdapter(mockedClient);
+
+        var requestInformationUrlTemplatePredicate = RequestInformationUrlTemplatePredicate(
+            urlTemplate
+        );
+        var requestInformationPredicate =
+            requestInfoPredicate != null
+                ? requestInfoPredicate.And(requestInformationUrlTemplatePredicate)
+                : requestInformationUrlTemplatePredicate;
+
+        requestAdapter
+            ?.SendAsync(
+                Arg.Is(requestInformationPredicate),
+                Arg.Any<ParsableFactory<R>>(),
+                Arg.Any<Dictionary<string, ParsableFactory<IParsable>>>(),
+                Arg.Any<CancellationToken>()
+            )
+            .Returns(Task.FromException<R?>(exception));
+    }
+
+    /// <summary>
+    /// Mocks an exception for a Kiota client request that returns no content.
+    /// </summary>
+    /// <typeparam name="T">The type of the Kiota client request builder.</typeparam>
+    /// <param name="mockedClient">The mocked client request builder instance.</param>
+    /// <param name="urlTemplate">The URL template to match the request.</param>
+    /// <param name="exception">The exception to throw when the request is made.</param>
+    /// <param name="requestInfoPredicate">
+    /// An optional predicate to further filter the request information.
+    /// </param>
+    public static void MockClientNoContentResponseException<T>(
+        this T mockedClient,
+        string urlTemplate,
+        Exception exception,
+        Expression<Predicate<RequestInformation>>? requestInfoPredicate = null
+    )
+        where T : BaseRequestBuilder
+    {
+        var requestAdapter = GetRequestAdapter(mockedClient);
+
+        var requestInformationUrlTemplatePredicate = RequestInformationUrlTemplatePredicate(
+            urlTemplate
+        );
+        var requestInformationPredicate =
+            requestInfoPredicate != null
+                ? requestInfoPredicate.And(requestInformationUrlTemplatePredicate)
+                : requestInformationUrlTemplatePredicate;
+
+        requestAdapter
+            ?.SendNoContentAsync(
+                Arg.Is(requestInformationPredicate),
+                Arg.Any<Dictionary<string, ParsableFactory<IParsable>>>(),
+                Arg.Any<CancellationToken>()
+            )
+            .Returns(Task.FromException(exception));
+    }
+
+    /// <summary>
+    /// Mocks an exception for a Kiota client request that returns a collection.
+    /// </summary>
+    /// <typeparam name="T">The type of the Kiota client request builder.</typeparam>
+    /// <typeparam name="R">The type of the response objects in the collection.</typeparam>
+    /// <param name="mockedClient">The mocked client request builder instance.</param>
+    /// <param name="urlTemplate">The URL template to match the request.</param>
+    /// <param name="exception">The exception to throw when the request is made.</param>
+    /// <param name="requestInfoPredicate">
+    /// An optional predicate to further filter the request information.
+    /// </param>
+    public static void MockClientCollectionResponseException<T, R>(
+        this T mockedClient,
+        string urlTemplate,
+        Exception exception,
+        Expression<Predicate<RequestInformation>>? requestInfoPredicate = null
+    )
+        where T : BaseRequestBuilder
+        where R : IParsable
+    {
+        var requestAdapter = GetRequestAdapter(mockedClient);
+
+        var requestInformationUrlTemplatePredicate = RequestInformationUrlTemplatePredicate(
+            urlTemplate
+        );
+        var requestInformationPredicate =
+            requestInfoPredicate != null
+                ? requestInfoPredicate.And(requestInformationUrlTemplatePredicate)
+                : requestInformationUrlTemplatePredicate;
+
+        requestAdapter
+            ?.SendCollectionAsync(
+                Arg.Is(requestInformationPredicate),
+                Arg.Any<ParsableFactory<R>>(),
+                Arg.Any<Dictionary<string, ParsableFactory<IParsable>>>(),
+                Arg.Any<CancellationToken>()
+            )
+            .Returns(Task.FromException<IEnumerable<R>?>(exception));
+    }
+
+    /// <summary>
+    /// Mocks an exception for a Kiota client request that returns a string.
+    /// </summary>
+    /// <typeparam name="T">The type of the Kiota client request builder.</typeparam>
+    /// <param name="mockedClient">The mocked client request builder instance.</param>
+    /// <param name="urlTemplate">The URL template to match the request.</param>
+    /// <param name="exception">The exception to throw when the request is made.</param>
+    /// <param name="requestInfoPredicate">
+    /// An optional predicate to further filter the request information.
+    /// </param>
+    public static void MockClientResponseException<T>(
+        this T mockedClient,
+        string urlTemplate,
+        Exception exception,
+        Expression<Predicate<RequestInformation>>? requestInfoPredicate = null
+    )
+        where T : BaseRequestBuilder
+    {
+        var requestAdapter = GetRequestAdapter(mockedClient);
+
+        var requestInformationUrlTemplatePredicate = RequestInformationUrlTemplatePredicate(
+            urlTemplate
+        );
+        var requestInformationPredicate =
+            requestInfoPredicate != null
+                ? requestInfoPredicate.And(requestInformationUrlTemplatePredicate)
+                : requestInformationUrlTemplatePredicate;
+
+        requestAdapter
+            ?.SendPrimitiveAsync<string>(
+                Arg.Is(requestInformationPredicate),
+                Arg.Any<Dictionary<string, ParsableFactory<IParsable>>>(),
+                Arg.Any<CancellationToken>()
+            )
+            .Returns(Task.FromException<string?>(exception));
+    }
 }
